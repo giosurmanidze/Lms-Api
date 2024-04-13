2669c3228179b2b89d80e07c7efb85938a07b9f5
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-04-13 21:09:39 +0400

    feat/protect routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-    
-Route::apiResource('/books', BookController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('/books', BookController::class);
+});
